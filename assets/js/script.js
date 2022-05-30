ca210a3b3ba13ceb5f1811461e91fcd5390b1eeb
martin-mcinerney <martin_mcinerney@live.ie>
2022-05-30 10:05:47 +0100

    Addition mode completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         for (let button of buttons) {
                 button.addEventListener("click", function () {
                         if (this.getAttribute("data-type") === "submit") {
-                                alert("You clicked Submit!");
+                                checkAnswer();
                         } else {
                                 let gameType = this.getAttribute("data-type");
                                 runGame(gameType);
@@ -32,13 +32,50 @@ function runGame(gameType) {
         }
 }
 
-
+/**This will check if the answer is correct */
 function checkAnswer() {
-
+        let userAnswer = parseInt(document.getElementById('answer-box').value);
+        let calculatedAnswer = calculateCorrectAnswer();
+        let isCorrect = userAnswer === calculatedAnswer[0];
+        if (isCorrect) {
+                alert('Hey! You got it right :)')
+        } else {
+                alert(`Sorry, you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}`);
+        }
 }
 
+/**Gets operands directly from the dom and calculates the correct answer */
 function calculateCorrectAnswer() {
+        let operand1 = parseInt(document.getElementById('operand1').innerText);
+        let operand2 = parseInt(document.getElementById('operand2').innerText);
+        let operator = document.getElementById('operator').innerText;
+        if (operator === '+') {
+                return [operand1 + operand2, "addition"];
+        } else {
+                alert(`Unimplemented operator: ${operator}`);
+                throw `Unimplemented operator: ${operator}. Aborting!`
+        };
 
+        if (operator === '-') {
+                return [operand1 - operand2];
+        } else {
+                alert(`Unimplemented operator: ${operator}`);
+                throw `Unimplemented operator: ${operator}. Aborting!`
+        };
+
+        if (operator === 'x') {
+                return [operand1 * operand2];
+        } else {
+                alert(`Unimplemented operator: ${operator}`);
+                throw `Unimplemented operator: ${operator}. Aborting!`
+        };
+
+        if (operator === '/') {
+                return [operand1 / operand2];
+        } else {
+                alert(`Unimplemented operator: ${operator}`);
+                throw `Unimplemented operator: ${operator}. Aborting!`
+        };
 }
 
 function incrementScore() {
@@ -57,9 +94,19 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion() {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = 'x';
+}
 
+function displayMultiplyQuestion() {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = '/';
 }
