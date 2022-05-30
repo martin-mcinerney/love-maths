639d4752852f4b44bff649b2bc8b3332a3b3079d
martin-mcinerney <martin_mcinerney@live.ie>
2022-05-30 10:21:01 +0100

    Added increment score function and decrement

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,10 @@ function checkAnswer() {
         let isCorrect = userAnswer === calculatedAnswer[0];
         if (isCorrect) {
                 alert('Hey! You got it right :)')
+                incrementScore();
         } else {
                 alert(`Sorry, you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}`);
+                incrementWrongAnswer()
         }
 }
 
@@ -77,13 +79,16 @@ function calculateCorrectAnswer() {
                 throw `Unimplemented operator: ${operator}. Aborting!`
         };
 }
-
+/**Increments the score in the dom by 1 */
 function incrementScore() {
+        let oldScore = parseInt(document.getElementById('score').innerText);
+        document.getElementById('score').innerText = ++oldScore;
 
 }
 
 function incrementWrongAnswer() {
-
+        let oldScore = parseInt(document.getElementById('incorrect').innerText);
+        document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
